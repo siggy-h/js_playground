384d5c012a0ae67f613cfda9198225f7931fc6da
sh <siegby@yahoo.com>
2016-04-22 15:44:19 -0700

    adding two exercise files

---
+++ js/testFun.js
@@ -0,0 +1,15 @@
+// test javascript file ;)
+
+function moo() {
+  document.writeln("\nMoooOOOooo");
+}
+
+function convert(x, y, z) {
+
+  var a = [];
+  a.push(x.toString());
+  a.push(y.toString());
+  a.push(z.toString());
+
+  return a;
+}
